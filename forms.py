c6605415244183781b31a0d64641a8d5dde4ef63
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2022-08-28 16:10:38 -0400

    Updated the server

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-from wtforms.fields.html5 import EmailField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, EmailField
+#from wtforms.fields.html5 import EmailField
 from wtforms.validators import InputRequired, Email
 
 
